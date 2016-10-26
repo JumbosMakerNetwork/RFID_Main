223143cff55624a2d9474975c7d80acc62828077
boconn7782 <brian.o_connell@tufts.edu>
2016-10-26 10:46:23 -0400

    Starting to reorganize the directory for better use.

--- compile1.sh
+++ compile1.sh
@@ -2,6 +2,6 @@
 # gcc *.c -Wall -Wextra -lcurl -lbcm2835 -lwiringPi -ljpeg -o rfid
 
 # Create object file from the source files
-gcc /home/rfid/TSync/headers/*.c -0 /home/rfid/TSync/headers/rfid_headers.o
+gcc /home/rfid/TSync/headers/*.c -o /home/rfid/TSync/headers/rfid_headers.o
 
 gcc /home/rfid/TSync/main.c /home/rfid/TSync/headers/rfid_headers.o -Wall -Wextra -lcurl -lbcm2835 -lwiringPi -ljpeg -o /home/rfid/TSync/rfid
