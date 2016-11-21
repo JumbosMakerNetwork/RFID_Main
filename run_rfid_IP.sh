ed417d2eb3d99c0043a44d965c9a52e379534b3c
boconn7782 <brian.o_connell@tufts.edu>
2016-11-21 12:30:23 -0500

    Adding autocheck for process running. Background script that checks every 60 seconds to see if the rfid application is running and restarts it if it isn't.

--- run_rfid_IP.sh
+++ run_rfid_IP.sh
@@ -6,14 +6,15 @@ SID=$1
 IPAdd=$(hostname -I)
 # sudo /home/rfid/TSync/rfid -STID $SID -MAC $MacAdd -IP $IPAdd &
 
-if [ "$2" = "-SI" ]; then
+# This eventually needs to be cleaner. Look through all for the "-SI" even though it will only ever be 2
+if [ -z "$2" ]; then
 	echo "Configuring as a SignIn Station"
-	stn = "SignIn"
-	cmd  = "sudo /home/rfid/TSync/SignIn -STID $SID -IP $IPAdd &"
+	stn="SignIn"
+	cmd="sudo /home/rfid/TSync/SignIn -STID $SID -IP $IPAdd &"
 else
 	echo "Configuring as an Interlock Terminal"
-	stn = "rfid"
-	cmd = "sudo /home/rfid/TSync/rfid -STID $SID -IP $IPAdd &"
+	stn="rfid"
+	cmd="sudo /home/rfid/TSync/rfid -STID $SID -IP $IPAdd &"
 fi
 
 echo $cmd
