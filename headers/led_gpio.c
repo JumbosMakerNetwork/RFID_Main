cc72e4127c3745a090e2185ffe76c1366f86ab16
boconn7782 <brian.o_connell@tufts.edu>
2016-11-23 12:44:34 -0500

    Serial listening attempt 2. Added try exception timeout.

--- headers/led_gpio.c
+++ headers/led_gpio.c
@@ -5,14 +5,15 @@
  */
 
 #include <errno.h>
-#include <stdlib.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <unistd.h>
 #include <wiringPi.h>
 #include <wiringSerial.h>
-#include <time.h>
-#include <unistd.h>
-#include <stdbool.h>
 #include "led_gpio.h"
 
 #define greenLED 29
@@ -70,12 +71,14 @@ void activate_LCD()
 
     while (AMA0_Feed) {
         t_last = time(NULL);
-        try {
+        try 
+        {
             int n = read(LCD, buff, sizeof(buff));
             printf("Waiting for quiet /dev/ttyAMA0 ... ");
             AMA0_Feed = true;
         }
-        catch (Exception ex) {
+        catch (Exception ex) 
+        {
             t_now = time(NULL);
             if (difftime(t_now, t_last) > timeout) {
                 printf("/dev/ttyAMA0 quiet ... ");
@@ -85,6 +88,35 @@ void activate_LCD()
         }
     }
 
+// #include <stdio.h>
+// #include <sys/types.h>
+// #include <sys/stat.h>
+// #include <fcntl.h>
+// #include <sys/select.h>
+
+// int main(void)
+// {
+//   fd_set set;
+//   struct timeval timeout;
+//   int rv;
+//   char buff[100];
+//   int len = 100;
+//   int filedesc = open( "dev/ttyS0", O_RDWR );
+
+//   FD_ZERO(&set); /* clear the set */
+//   FD_SET(filedesc, &set); /* add our file descriptor to the set */
+
+//   timeout.tv_sec = 0;
+//   timeout.tv_usec = 500;
+
+//   rv = select(filedesc + 1, &set, NULL, NULL, &timeout);
+//   if(rv == -1)
+//     perror("select"); /* an error accured */
+//   else if(rv == 0)
+//     printf("timeout"); /* a timeout occured */
+//   else
+//     read( filedesc, buff, len ); /* there was data to read */
+// }
 
     digitalWrite(LCD_3v3, HIGH); 
 
