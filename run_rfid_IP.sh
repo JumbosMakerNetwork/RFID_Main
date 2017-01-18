7912e16ee5f8f3eda354374f64f67afbd8db724c
boconn7782 <brian.o_connell@tufts.edu>
2017-01-18 14:06:22 -0500

    Reintroduced the wait for ping

--- run_rfid_IP.sh
+++ run_rfid_IP.sh
@@ -2,10 +2,10 @@
 # hostname -I | sudo /home/dolanwill/rfid/rfid -IP &
 # sudo sh /home/rfid/TSync/run_rfid_IP.sh 23
 
-# while ! ping -c 1 130.64.17.0 | grep rtt; do
-#     echo "Waiting for connection to network - network interface might be down..."
-#     sleep 2
-# done
+while ! ping -c 1 130.64.17.0 | grep rtt; do
+    echo "Waiting for connection to network - network interface might be down..."
+    sleep 2
+done
 
 # while ! [ $( ifconfig -s | grep eth0 ) ]; do
 #     echo "Waiting for connection to network - network interface might be down..."
