ab806c57e3b980cb6c03ffc55fce977962871926
boconn7782 <brian.o_connell@tufts.edu>
2017-01-18 15:46:15 -0500

    Added another minute to start up wait

--- run_rfid_IP.sh
+++ run_rfid_IP.sh
@@ -7,29 +7,16 @@ while ! ping -c 1 130.64.17.0 | grep rtt; do
     sleep 2
 done
 
-# while ! [ $( ifconfig -s | grep eth0 ) ]; do
-#     echo "Waiting for connection to network - network interface might be down..."
-#     sleep 2
-# done
-
-sleep 60
+sleep 120
 SID=$1
-# MacAdd=$(cat /sys/class/net/wlan0/address)
 IPAdd=$(hostname -I)
-# sudo /home/rfid/TSync/rfid -STID $SID -MAC $MacAdd -IP $IPAdd &
 
 # This eventually needs to be cleaner. Look through all for the "-SI" even though it will only ever be 2
 if [ "$2" = "-SI" ]; then
 	echo "Configuring as a SignIn Station"
-	# stn="SignIn"
-	# cmd="sudo /home/rfid/TSync/SignIn -STID $SID -IP $IPAdd &"
 	sudo /home/rfid/TSync/SignIn -STID $SID -IP $IPAdd &
-	# sudo sh /home/rfid/TSync/ProcessChk.sh $SID -SI &
 else
 	echo "Configuring as an Interlock Terminal"
-	# stn="rfid"
-	# cmd="sudo /home/rfid/TSync/rfid -STID $SID -IP $IPAdd &"
 	sudo /home/rfid/TSync/rfid -STID $SID -IP $IPAdd &
-	# sudo sh /home/rfid/TSync/ProcessChk.sh $SID &
 fi
 
